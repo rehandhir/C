a611c4103769a7e0222702f61be4441522790c8d
Hharshitsingh <realharsgitsingh@gmail.com>
2020-07-05 15:52:41 +0530

    update

--- C program/15.ASCII message.c
+++ C program/15.ASCII message.c
@@ -1,16 +1,7 @@
-#include<windows.h>
-void gotoxy(int x, int y)
-{
-    COORD c;
-    c.X=x;
-    c.Y=y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
-}
+#include<stdio.h>
  char main()
 {
    int a=65,b=71,c=72,d=73,e=78,f=82,g=83,h=84;
-     gotoxy(31,12);
     printf("%c%c%c%c%c%c%c",c,a,f,g,c,d,h);
-    gotoxy(31,13);
-    printf("%c%c%c%c%c",g,d,e,b,c);
+    printf(" %c%c%c%c%c\n",g,d,e,b,c);
 }
--- C program/16.ASCII.c
+++ C program/16.ASCII.c
@@ -3,7 +3,7 @@ char main()
 {
     int a,b,c,d,e,f,g,h;
     printf("Enter  numbers:");
-    scanf("%d,%d,%d,%d,%d,%d,%d,%d", &a,&b,&c,&d,&e,&f,&g,&h);
+    scanf("%d %d %d %d %d %d %d %d", &a,&b,&c,&d,&e,&f,&g,&h);
     printf("%c%c%c%c%c%c%c\t%c%c%c%c%c",c,a,f,g,c,d,h,g,d,e,b,c);
     return 0;
 }
